c1c138150cdf4d0f6ac57e4919ce1ddd61a43c01
KanekiEzz <ilyassezzam1337@gmail.com>
2024-10-09 15:23:47 +0100

    add header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,21 @@
 import './App.css';
 
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+
 import HomePage from './pages/HomePage'
 import LoginPage from './pages/LoginPage'
+import Header from './components/Header'
 
 function App() {
   return (
     <div className='App'>
-      <HomePage />
-      <LoginPage />
-      <p>Kaneki</p>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/login' element={<LoginPage />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
